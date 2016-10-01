fa0c62d302d19f20ee162dcbe464530eeb643a64
han <caokyhan@gmail.com>
2016-10-01 13:00:10 +0700

    copy proxyServer.js to index.js and remove file proxyServer.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let http = require('http')
+let url = require('url')
+let argv = require('yargs')
+    .default('host', '127.0.0.1:8000')
+    .argv
 // echo server
 http.createServer((req, res) => {
     console.log(`Request received at: ${req.url}`)
@@ -7,3 +11,37 @@ http.createServer((req, res) => {
     }
     req.pipe(res)
 }).listen(8000)
+
+// proxy server
+let request = require('request')
+let path = require('path')
+let fs = require('fs')
+let logPath = argv.log && path.join(__dirname, argv.log)
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
+
+let scheme = 'http://'
+let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
+// update destinationUrl
+let destinationUrl = argv.url|| url.format({
+       protocol: 'http',
+       host: argv.host,
+       port
+    })
+// proxy server
+http.createServer((req,res)=>{
+	// allow the x-destination-url header to override the destinationUrl value
+	destinationUrl = req.headers['x-destination-url']? req.headers['x-destination-url']:destinationUrl
+    console.log(`Proxying request to: ${destinationUrl + req.url}`)
+	// Proxy code
+    let options = {
+        header: req.headers,
+        url: `${destinationUrl}${req.url}`
+    }
+    options.method = req.method
+    let outboundResponse = request(options)
+    req.pipe(outboundResponse)
+
+    req.pipe(logStream, {end: false})
+	logStream.write('Request headers: ' + JSON.stringify(req.headers))
+    outboundResponse.pipe(res)
+}).listen(8001)
